aa80a0b97b94c53eedfd5cec7994eda1513e91bf
Anand <anand@igi.tugraz.at>
2017-03-15 20:23:46 +0100

    Exiting with status of flake8

--- run-style-check.sh
+++ run-style-check.sh
@@ -1,5 +1,7 @@
 mkdir style-reports
-flake8 --ignore=W293,W291,E265,E231,E127,E262 --max-line-length=120 | tee style-reports/report.txt
+now=$(date +"%m-%d-%Y-%T")
+flake8 --ignore=W293,W291,E265,E231,E127,E262 --max-line-length=120 --exclude style-reports/ --tee --output-file style-reports/report-${now}.txt
+# echo $?
 # E501 line too long (89 > 79 characters)
 # W293 blank line contains whitespace
 # W291 trailing whitespace
@@ -7,3 +9,4 @@ flake8 --ignore=W293,W291,E265,E231,E127,E262 --max-line-length=120 | tee style-
 # E231 missing whitespace after ':'
 # E127 continuation line over-indented for visual indent
 # E262 inline comment should start with '# '
+exit $?
