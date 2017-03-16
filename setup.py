de924192eac5c28bedbfc7f9e2e597f403c38adf
Arjun Rao <arjun210493@gmail.com>
2017-03-17 00:34:24 +0100

    Added setup.py for version 0.1.0 + changed README.rst

---
+++ setup.py
@@ -0,0 +1,22 @@
+# This file installs the ltl package. Note that it does not perform any installation of the documentation. For this, follow the specified procedure in the README
+from setuptools import setup, find_packages
+setup(
+    name="learntolearn",
+    version="0.1.0",
+    packages=['ltl'],
+    author="Arjun Rao",
+    author_email="arjun@igi.tugraz.at",
+    description="This module provides the infrastructure create optimizers and "
+                "optimizees in order to implement learning-to-learn",
+    license="MIT",
+    keywords="Generic Builder builder generic",
+    install_requires=['numpy',
+                      'scipy',
+                      'pyyaml',
+                      'matplotlib',
+                      'pypet==0.4.0',
+                      'scoop==0.7.1.1',
+                      'sphinx',
+                      'deap'],
+    provides=['ltl'],
+)
