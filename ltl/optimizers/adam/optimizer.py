fixed wrong calculation of gradient

ltl/optimizers/adam/optimizer.py
@@ -23,7 +23,7 @@ AdamParameters.__doc__ = """
 """
 
 
-class GradientDescentOptimizer(Optimizer):
+class AdamOptimizer(Optimizer):
     """
     Class for a generic ADAM gradient descent solver.
     In the pseudo code the algorithm does:
@@ -124,7 +124,7 @@ class GradientDescentOptimizer(Optimizer):
         assert len(fitnesses_results) - 1 == traj.n_random_steps
 
         # We need to collect the directions of the random steps along with the fitness evaluated there
-        dx = np.zeros(n_random_steps)
+        dx = np.zeros((n_random_steps, len(self.current_individual))
         weighted_fitness = np.zeros(n_random_steps)
 
         for i, (run_index, fitness) in enumerate(fitnesses_results):
@@ -151,7 +151,7 @@ class GradientDescentOptimizer(Optimizer):
 
         if self.g < n_iteration - 1 and stop_criterion > self.current_fitness:
             # Update momentum estimates and calculate gradient 
-            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness)
+            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness - self.current_fitness)
             self.s = first_order_decay * self.s + (1 - first_order_decay) * gradient
             self.r = second_order_decay * self.r + (1 - second_order_decay) * np.multiply(gradient, gradient)
             s_corrected = self.s / (1 - first_order_decay ^ g)

ltl/optimizers/gd/optimizer.py
@@ -113,7 +113,7 @@ class GradientDescentOptimizer(Optimizer):
         assert len(fitnesses_results) - 1 == traj.n_random_steps
 
         # We need to collect the directions of the random steps along with the fitness evaluated there
-        dx = np.zeros(n_random_steps)
+        dx = np.zeros((n_random_steps, len(self.current_individual))
         weighted_fitness = np.zeros(n_random_steps)
 
         for i, (run_index, fitness) in enumerate(fitnesses_results):
@@ -140,7 +140,7 @@ class GradientDescentOptimizer(Optimizer):
 
         if self.g < n_iteration - 1 and stop_criterion > self.current_fitness:
             # Create new individual using gradient descent
-            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness)
+            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness - self.current_fitness)
             current_individual += traj.learning_rate * gradient
             if optimizee_bounding_func is not None:
                 current_individual = self.optimizee_bounding_func(current_individual)

ltl/optimizers/rmsprop/optimizer.py
@@ -120,7 +120,7 @@ class RMSPropOptimizer(Optimizer):
         assert len(fitnesses_results) - 1 == traj.n_random_steps
 
         # We need to collect the directions of the random steps along with the fitness evaluated there
-        dx = np.zeros(n_random_steps)
+        np.zeros((n_random_steps, len(self.current_individual))
         weighted_fitness = np.zeros(n_random_steps)
 
         for i, (run_index, fitness) in enumerate(fitnesses_results):
@@ -147,7 +147,7 @@ class RMSPropOptimizer(Optimizer):
 
         if self.g < n_iteration - 1 and stop_criterion > self.current_fitness:
             # Update momentum estimates and calculate gradient 
-            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness)
+            gradient = np.dot(np.linalg.pinv(dx), weighted_fitness - self.current_fitness)
             self.r = momentum_decay * self.r + (1 - momentum_decay) * np.multiply(gradient, gradient)
 
             current_individual += np.multiply(traj.learning_rate / (np.sqrt(self.r + self.delta), gradient)