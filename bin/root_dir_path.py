35b3ae7cb56b0581e838cccc497aac8581416e3e
Darjan Salaj <salaj@gmx.at>
2017-04-19 18:32:03 +0200

    Added comment for better usability

--- bin/root_dir_path.py
+++ bin/root_dir_path.py
@@ -1,1 +1,3 @@
+# If you don't want git to see the changes of this file run the this:
+# git update-index --assume-unchanged bin/root_dir_path.py
 root_dir_path = None  # CHANGE THIS to the directory where your simulation results are contained
