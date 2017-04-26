13600e1086fd2169866e24a7b0d345ce8f1214ea
Sinisa Stekovic <sinisa.stekovic@student.tugraz.at>
2017-04-26 10:10:28 +0200

    Changed benchmark_functions to be able to parse recorder params

--- ltl/optimizees/functions/benchmarked_functions.py
+++ ltl/optimizees/functions/benchmarked_functions.py
@@ -11,76 +11,94 @@ class BenchmarkedFunctions:
     :param mu: Scalar indicating the mean of the Gaussian noise.
     :param sigma: Scalar indicating the standard deviation of the Gaussian noise.
     """
-    def __init__(self, noise=False, mu=0., sigma=0.01):
-        self.function_name_map = [("Rastrigin2d", self._create_rastrigin2d(noise, mu, sigma)),
-                                  ("Rastrigin10d", self._create_rastrigin10d(noise, mu, sigma)),
-                                  ("Rosenbrock2d", self._create_rosenbrock2d(noise, mu, sigma)),
-                                  ("Rosenbrock10d", self._create_rosenbrock10d(noise, mu, sigma)),
-                                  ("Ackley2d", self._create_ackley2d(noise, mu, sigma)),
-                                  ("Ackley10d", self._create_ackley10d(noise, mu, sigma)),
-                                  ("Chasm", self._create_chasm(noise, mu, sigma)),
-                                  ("Gauss2d", self._create_gauss2d(noise, mu, sigma)),
-                                  ("Shekel2d", self._create_shekel2d(noise, mu, sigma)),
-                                  ("Langermann2d", self._create_langermann(noise, mu, sigma)),
-                                  ("Michalewicz2d", self._create_michalewicz2d(noise, mu, sigma)),
-                                  ("Permutation2d", self._create_permutation2d(noise, mu, sigma)),
-                                  ("Easom2d", self._create_easom2d(noise, mu, sigma)),
-                                  ("Easom10d", self._create_easom10d(noise, mu, sigma)),
-                                  ("3Gaussians2d", self._create_3gaussians2d(noise, mu, sigma))]
+    def __init__(self):
+        self.function_name_map = [("Rastrigin2d", self._create_rastrigin2d()),
+                                  ("Rastrigin10d", self._create_rastrigin10d()),
+                                  ("Rosenbrock2d", self._create_rosenbrock2d()),
+                                  ("Rosenbrock10d", self._create_rosenbrock10d()),
+                                  ("Ackley2d", self._create_ackley2d()),
+                                  ("Ackley10d", self._create_ackley10d()),
+                                  ("Chasm", self._create_chasm()),
+                                  ("Gauss2d", self._create_gauss2d()),
+                                  ("Shekel2d", self._create_shekel2d()),
+                                  ("Langermann2d", self._create_langermann()),
+                                  ("Michalewicz2d", self._create_michalewicz2d()),
+                                  ("Permutation2d", self._create_permutation2d()),
+                                  ("Easom2d", self._create_easom2d()),
+                                  ("Easom10d", self._create_easom10d()),
+                                  ("3Gaussians2d", self._create_3gaussians2d())]
 
-    def get_function_by_index(self, id):
+    def get_function_by_index(self, id, noise=False, mu=0., sigma=0.01):
         """
         Get the benchmarked function with given id
         :param id: Function id
+        :param noise: Indicates whether the function should provide noisy values
+        :param mu: mean of the noise
+        :param sigma: convariance of the noise
+
+        :return function_name_map entry for the given id and the parameters for the benchmark
         """
-        return self.function_name_map[id]
 
-    def _create_rastrigin2d(self, noise, mu, sigma):
-        return FunctionGenerator([RastriginParameters()], dims=2, noise=noise, mu=mu, sigma=sigma)
+        #first update the noise for the given function
+        self.function_name_map[id][1].noise = noise
+        self.function_name_map[id][1].mu = mu
+        self.function_name_map[id][1].sigma = sigma
 
-    def _create_rastrigin10d(self, noise, mu, sigma):
-        return FunctionGenerator([RastriginParameters()], dims=10, noise=noise, mu=mu, sigma=sigma)
+        return self.function_name_map[id], self._parse_parameters(id, noise, mu, sigma)
 
-    def _create_rosenbrock2d(self, noise, mu, sigma):
-        return FunctionGenerator([RosenbrockParameters()], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _parse_parameters(self, id, noise, mu, sigma):
+        if noise:
+            params_dict = {"benchmark_id": id,
+                           "mu": mu,
+                           "sigma": sigma}
+        else:
+            params_dict = {"id": id}
+        return params_dict
 
-    def _create_rosenbrock10d(self, noise, mu, sigma):
-        return FunctionGenerator([RosenbrockParameters()], dims=10, noise=noise, mu=mu, sigma=sigma)
+    def _create_rastrigin2d(self):
+        return FunctionGenerator([RastriginParameters()], dims=2)
 
-    def _create_ackley2d(self, noise, mu, sigma):
-        return FunctionGenerator([AckleyParameters()], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_rastrigin10d(self):
+        return FunctionGenerator([RastriginParameters()], dims=10)
 
-    def _create_ackley10d(self, noise, mu, sigma):
-        return FunctionGenerator([AckleyParameters()], dims=10, noise=noise, mu=mu, sigma=sigma)
+    def _create_rosenbrock2d(self):
+        return FunctionGenerator([RosenbrockParameters()], dims=2)
 
-    def _create_chasm(self, noise, mu, sigma):
-        return FunctionGenerator([ChasmParameters()], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_rosenbrock10d(self):
+        return FunctionGenerator([RosenbrockParameters()], dims=10)
 
-    def _create_gauss2d(self, noise, mu, sigma):
-        return FunctionGenerator([GaussianParameters(sigma=[[1.5, .1], [.1, .3]], mean=[-1., -1.])], dims=2,
-                                                    noise=noise, mu=mu, sigma=sigma)
+    def _create_ackley2d(self):
+        return FunctionGenerator([AckleyParameters()], dims=2)
 
-    def _create_shekel2d(self, noise, mu, sigma):
-        return FunctionGenerator([ShekelParameters(A='default', c='default')], dims=2, noise=noise,
-                                 mu=mu, sigma=sigma)
+    def _create_ackley10d(self):
+        return FunctionGenerator([AckleyParameters()], dims=10)
 
-    def _create_michalewicz2d(self, noise, mu, sigma):
-        return FunctionGenerator([MichalewiczParameters(m='default')], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_chasm(self):
+        return FunctionGenerator([ChasmParameters()], dims=2)
 
-    def _create_permutation2d(self, noise, mu, sigma):
-        return FunctionGenerator([PermutationParameters(beta=0.005)], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_gauss2d(self):
+        return FunctionGenerator([GaussianParameters(sigma=[[1.5, .1], [.1, .3]], mean=[-1., -1.])], dims=2)
 
-    def _create_easom2d(self, noise, mu, sigma):
-        return FunctionGenerator([EasomParameters()], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_shekel2d(self):
+        return FunctionGenerator([ShekelParameters(A='default', c='default')], dims=2)
 
-    def _create_easom10d(self, noise, mu, sigma):
-        return FunctionGenerator([EasomParameters()], dims=10, noise=noise, mu=mu, sigma=sigma)
+    def _create_michalewicz2d(self):
+        return FunctionGenerator([MichalewiczParameters(m='default')], dims=2)
 
-    def _create_langermann(self, noise, mu, sigma):
-        return FunctionGenerator([LangermannParameters(A='default', c='default')], dims=2, noise=noise, mu=mu, sigma=sigma)
+    def _create_permutation2d(self):
+        return FunctionGenerator([PermutationParameters(beta=0.005)], dims=2)
 
-    def _create_3gaussians2d(self, noise, mu, sigma):
+    def _create_easom2d(self):
+        return FunctionGenerator([EasomParameters()], dims=2)
+
+    def _create_easom10d(self):
+        return FunctionGenerator([EasomParameters()], dims=10)
+
+    def _create_langermann(self):
+        return FunctionGenerator([LangermannParameters(A='default', c='default')], dims=2)
+
+    def _create_3gaussians2d(self):
         fg_params = [GaussianParameters(sigma=[[1.5, .1], [.1, .3]], mean=[-1., -1.]),
                      GaussianParameters(sigma=[[.25, .3], [.3, 1.]], mean=[1., 1.]),
                      GaussianParameters(sigma=[[.5, .25], [.25, 1.3]], mean=[2., -2.])]
-        return FunctionGenerator(fg_params, dims=2, noise=noise, mu=mu, sigma=sigma)
+        return FunctionGenerator(fg_params, dims=2)
