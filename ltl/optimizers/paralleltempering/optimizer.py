c42ded972b062c99f3c993b1ef50be60051e9156
Daniel Hackhofer <daniel.hackhofer@student.tugraz.at>
2017-05-19 14:35:28 +0200

    debug

--- ltl/optimizers/paralleltempering/optimizer.py
+++ ltl/optimizers/paralleltempering/optimizer.py
@@ -148,9 +148,10 @@ class ParallelTemperingOptimizer(Optimizer):
         
         assert schedule_known, print("Warning: Unknown cooling schedule")
         
-        self.recorder_parameters = ParallelTemperingParameters(n_parallel_runs=parameters.n_parallel_runs, noisy_step=parameters.noisy_step, n_iteration=parameters.n_iteration, stop_criterion=parameters.stop_criterion,
-                                              seed=parameters.seed, cooling_schedules=cooling_schedules_string, 
-                                              temperature_bounds=temperature_bounds_string, decay_parameters=decay_parameters_string)
+#        self.recorder_parameters = ParallelTemperingParameters(n_parallel_runs=parameters.n_parallel_runs, noisy_step=parameters.noisy_step, n_iteration=parameters.n_iteration, stop_criterion=parameters.stop_criterion,
+#                                              seed=parameters.seed, cooling_schedules=cooling_schedules_string, 
+#                                              temperature_bounds=temperature_bounds_string, decay_parameters=decay_parameters_string)
+        self.recorder_parameters = parameters
         
     def cooling(self,temperature, cooling_schedule, decay_parameter, temperature_bounds, steps_total):        
         
