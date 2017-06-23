f3bc1d89323316908692cd739e91d59654e6da3a
maharjun <arjun210493@gmail.com>
2017-06-23 13:41:21 +0200

    Added Classes to the __init__.py of optimizees and optimizers (#89)
    Added all the classes of the optimizees and optimizers in their respective __init__.py files, removes the unnatural requirement of writing things like optimizers.crossentropy.optimizer. ...

---
+++ ltl/optimizers/evolution/__init__.py
@@ -0,0 +1,7 @@
+from .optimizer import GeneticAlgorithmParameters
+from .optimizer import GeneticAlgorithmOptimizer
+
+__all__ = [
+    'GeneticAlgorithmParameters',
+    'GeneticAlgorithmOptimizer',
+]
