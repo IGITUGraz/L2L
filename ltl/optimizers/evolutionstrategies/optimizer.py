5a3c08c239440b6d4098fe46b785ebe51279bfa3
Anand <anandtrex@users.noreply.github.com>
2017-11-02 20:25:48 +0100

    Making sure noise_std shape is valid

--- ltl/optimizers/evolutionstrategies/optimizer.py
+++ ltl/optimizers/evolutionstrategies/optimizer.py
@@ -134,6 +134,10 @@ class EvolutionStrategiesOptimizer(Optimizer):
 
         self.current_individual_arr, self.optimizee_individual_dict_spec = dict_to_list(
             self.optimizee_create_individual(), get_dict_spec=True)
+
+        noise_std_shape = np.array(parameters.noise_std).shape
+        assert noise_std_shape == () or noise_std_shape == self.current_individual_arr.shape
+
         traj.f_add_derived_parameter(
             'dimension',
             self.current_individual_arr.shape,
