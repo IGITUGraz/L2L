b5207e06faaaf48b90eeaca9dd65ba41833663c5
Jakob Jordan <j.jordan@fz-juelich.de>
2017-11-03 10:54:41 +0100

    Add natural evolution strategies optimizer
    Implements separable natural evolution strategies in natural
    coordinates for multinormal search distributions as described in
    Wierstra, D., Schaul, T., Glasmachers, T., Sun, Y., Peters, J., &
    Schmidhuber, J. (2014). Natural evolution strategies. Journal of
    Machine Learning Research, 15(1) (pp. 949-980).

---
+++ ltl/optimizers/naturalevolutionstrategies/__init__.py
@@ -0,0 +1,3 @@
+from .optimizer import NaturalEvolutionStrategiesOptimizer, NaturalEvolutionStrategiesParameters
+
+__all__ = ['NaturalEvolutionStrategiesOptimizer', 'NaturalEvolutionStrategiesParameters']
