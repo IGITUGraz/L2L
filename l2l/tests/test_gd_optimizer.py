7425ff1c867d8b2c976d3a3167ce942a5fccf476
Sandra Diaz <s.diaz@fz-juelich.de>
2020-09-16 10:13:31 +0200

    Changes to path setup

--- l2l/tests/test_gd_optimizer.py
+++ l2l/tests/test_gd_optimizer.py
@@ -2,12 +2,14 @@ import unittest
 
 import numpy as np
 from l2l.utils.environment import Environment
-from l2l.optimizers.evolutionstrategies import EvolutionStrategiesParameters, EvolutionStrategiesOptimizer
+from l2l.optimizers.gradientdescent.optimizer import GradientDescentOptimizer
+from l2l.optimizers.gradientdescent.optimizer import RMSPropParameters
 
 from l2l.optimizees.functions.benchmarked_functions import BenchmarkedFunctions
 from l2l.optimizees.functions.optimizee import FunctionGeneratorOptimizee
 
-class CEOptimizerTestCase(unittest.TestCase):
+
+class GDOptimizerTestCase(unittest.TestCase):
 
     def setUp(self):
         name = "test_trajectory"
@@ -20,7 +22,7 @@ class CEOptimizerTestCase(unittest.TestCase):
             automatic_storing=True,
             log_stdout=False,  # Sends stdout to logs
         )
-        self.traj = self.env.trajectory
+        self.trajectory = self.env.trajectory
         ## Benchmark function
         function_id = 14
         bench_functs = BenchmarkedFunctions()
@@ -28,26 +30,18 @@ class CEOptimizerTestCase(unittest.TestCase):
             bench_functs.get_function_by_index(function_id, noise=True)
 
         optimizee_seed = 1
-        self.optimizee = FunctionGeneratorOptimizee(self.traj, benchmark_function, seed=optimizee_seed)
+        self.optimizee = FunctionGeneratorOptimizee(self.trajectory, benchmark_function, seed=optimizee_seed)
 
     def test_setup(self):
 
-        parameters = EvolutionStrategiesParameters(
-        learning_rate=0.1,
-        noise_std=1.0,
-        mirrored_sampling_enabled=True,
-        fitness_shaping_enabled=True,
-        pop_size=1,
-        n_iteration=1,
-        stop_criterion=np.Inf,
-        seed=1)
+        parameters = RMSPropParameters(learning_rate=0.01, exploration_step_size=0.01,
+                                       n_random_steps=5, momentum_decay=0.5,
+                                       n_iteration=100, stop_criterion=np.Inf, seed=99)
 
-        optimizer = EvolutionStrategiesOptimizer(
-        self.traj,
-        optimizee_create_individual=self.optimizee.create_individual,
-        optimizee_fitness_weights=(-1.,),
-        parameters=parameters,
-        optimizee_bounding_func=self.optimizee.bounding_func)
+        optimizer = GradientDescentOptimizer(self.trajectory, optimizee_create_individual=self.optimizee.create_individual,
+                                             optimizee_fitness_weights=(0.1,),
+                                             parameters=parameters,
+                                             optimizee_bounding_func=self.optimizee.bounding_func)
 
         self.assertIsNotNone(optimizer.parameters)
         try:
@@ -57,7 +51,7 @@ class CEOptimizerTestCase(unittest.TestCase):
 
 
 def suite():
-    suite = unittest.makeSuite(CEOptimizerTestCase, 'test')
+    suite = unittest.makeSuite(GDOptimizerTestCase, 'test')
     return suite
 
 
