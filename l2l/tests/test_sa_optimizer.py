6cb1e8f51242311fac26b8f04e59e2b97c18e9ce
Sandra Diaz <s.diaz@fz-juelich.de>
2020-09-17 22:24:54 +0200

    Fixed test SA

--- l2l/tests/test_sa_optimizer.py
+++ l2l/tests/test_sa_optimizer.py
@@ -6,11 +6,21 @@ from l2l.optimizers.simulatedannealing.optimizer import SimulatedAnnealingParame
 
 from l2l.optimizees.functions.benchmarked_functions import BenchmarkedFunctions
 from l2l.optimizees.functions.optimizee import FunctionGeneratorOptimizee
+from l2l.utils import JUBE_runner as jube
+from l2l.paths import Paths
+import os
 
-class SACEOptimizerTestCase(unittest.TestCase):
+
+class SAOptimizerTestCase(unittest.TestCase):
 
     def setUp(self):
         name = "test_trajectory"
+        try:
+            with open('../../bin/path.conf') as f:
+                root_dir_path = f.read().strip()
+        except FileNotFoundError:
+            self.fail("L2L is not well configured. Missing path file.")
+        self.paths = Paths(name, dict(run_num='test'), root_dir_path=root_dir_path, suffix="-" + name)
         self.env = Environment(
             trajectory=name,
             filename=".",
@@ -21,7 +31,11 @@ class SACEOptimizerTestCase(unittest.TestCase):
             log_stdout=False,  # Sends stdout to logs
         )
         self.trajectory = self.env.trajectory
-        ## Benchmark function
+        self.trajectory.f_add_parameter_group("JUBE_params", "Contains JUBE parameters")
+        self.trajectory.f_add_parameter_to_group("JUBE_params", "exec", "python3 " +
+                                      os.path.join(self.paths.simulation_path, "run_files/run_optimizee.py"))
+        self.trajectory.f_add_parameter_to_group("JUBE_params", "paths", self.paths)
+        # Test function
         function_id = 14
         bench_functs = BenchmarkedFunctions()
         (benchmark_name, benchmark_function), benchmark_parameters = \
@@ -29,10 +43,11 @@ class SACEOptimizerTestCase(unittest.TestCase):
 
         optimizee_seed = 1
         self.optimizee = FunctionGeneratorOptimizee(self.trajectory, benchmark_function, seed=optimizee_seed)
+        jube.prepare_optimizee(self.optimizee, self.paths.simulation_path)
 
     def test_setup(self):
 
-        parameters = SimulatedAnnealingParameters(n_parallel_runs=50, noisy_step=.03, temp_decay=.99, n_iteration=100,
+        parameters = SimulatedAnnealingParameters(n_parallel_runs=1, noisy_step=.03, temp_decay=.99, n_iteration=1,
                                                   stop_criterion=np.Inf, seed=np.random.randint(1e5),
                                                   cooling_schedule=AvailableCoolingSchedules.QUADRATIC_ADDAPTIVE)
 
@@ -42,10 +57,12 @@ class SACEOptimizerTestCase(unittest.TestCase):
                                                 optimizee_bounding_func=self.optimizee.bounding_func)
 
         self.assertIsNotNone(optimizer.parameters)
+        self.env.add_postprocessing(optimizer.post_process)
         try:
-            optimizer.post_process()
+            self.env.run(self.optimizee.simulate)
         except Exception:
-            self.fail()
+            self.fail(Exception.__name__)
+        optimizer.end(self.trajectory)
 
 
 def suite():
