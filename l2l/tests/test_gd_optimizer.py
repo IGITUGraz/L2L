2ff91635dd9535eabab3737256c46f48d82b2e5d
Sandra Diaz <s.diaz@fz-juelich.de>
2020-09-17 23:37:38 +0200

    Adjusting tests to use experiment object

--- l2l/tests/test_gd_optimizer.py
+++ l2l/tests/test_gd_optimizer.py
@@ -1,53 +1,41 @@
 import unittest
 
 import numpy as np
-from l2l.utils.environment import Environment
 from l2l.optimizers.gradientdescent.optimizer import GradientDescentOptimizer
 from l2l.optimizers.gradientdescent.optimizer import RMSPropParameters
+from .test_optimizer import OptimizerTestCase
 
-from l2l.optimizees.functions.benchmarked_functions import BenchmarkedFunctions
-from l2l.optimizees.functions.optimizee import FunctionGeneratorOptimizee
+from l2l import list_to_dict
 
 
-class GDOptimizerTestCase(unittest.TestCase):
+class GDOptimizerTestCase(OptimizerTestCase):
 
-    def setUp(self):
-        name = "test_trajectory"
-        self.env = Environment(
-            trajectory=name,
-            filename=".",
-            file_title='{} data'.format(name),
-            comment='{} data'.format(name),
-            add_time=True,
-            automatic_storing=True,
-            log_stdout=False,  # Sends stdout to logs
-        )
-        self.trajectory = self.env.trajectory
-        ## Benchmark function
-        function_id = 14
-        bench_functs = BenchmarkedFunctions()
-        (benchmark_name, benchmark_function), benchmark_parameters = \
-            bench_functs.get_function_by_index(function_id, noise=True)
+    def test_gd(self):
+        optimizer_parameters = RMSPropParameters(learning_rate=0.01, exploration_step_size=0.01,
+                                       n_random_steps=1, momentum_decay=0.5,
+                                       n_iteration=1, stop_criterion=np.Inf, seed=99)
 
-        optimizee_seed = 1
-        self.optimizee = FunctionGeneratorOptimizee(self.trajectory, benchmark_function, seed=optimizee_seed)
-
-    def test_setup(self):
-
-        parameters = RMSPropParameters(learning_rate=0.01, exploration_step_size=0.01,
-                                       n_random_steps=5, momentum_decay=0.5,
-                                       n_iteration=100, stop_criterion=np.Inf, seed=99)
-
-        optimizer = GradientDescentOptimizer(self.trajectory, optimizee_create_individual=self.optimizee.create_individual,
+        optimizer = GradientDescentOptimizer(self.trajectory,
+                                             optimizee_create_individual=self.optimizee.create_individual,
                                              optimizee_fitness_weights=(0.1,),
-                                             parameters=parameters,
+                                             parameters=optimizer_parameters,
                                              optimizee_bounding_func=self.optimizee.bounding_func)
-
         self.assertIsNotNone(optimizer.parameters)
+
         try:
-            optimizer.post_process()
+
+            self.experiment.run_experiment(optimizee=self.optimizee,
+                                  optimizee_parameters=self.optimizee_parameters,
+                                  optimizer=optimizer,
+                                  optimizer_parameters=optimizer_parameters)
         except Exception:
-            self.fail()
+            self.fail(Exception.__name__)
+        print(self.experiment.optimizer)
+        best = list_to_dict(self.experiment.optimizer.current_individual.tolist(),
+                             self.experiment.optimizer.optimizee_individual_dict_spec)['coords']
+        self.assertEqual(best[0],-4.998856251826551)
+        self.assertEqual(best[1],-1.9766742736816023)
+        self.experiment.end_experiment(optimizer)
 
 
 def suite():
--- l2l/tests/test_sa_optimizer.py
+++ l2l/tests/test_sa_optimizer.py
@@ -1,69 +1,31 @@
 import unittest
 
+from .test_optimizer import OptimizerTestCase
 import numpy as np
-from l2l.utils.environment import Environment
 from l2l.optimizers.simulatedannealing.optimizer import SimulatedAnnealingParameters, SimulatedAnnealingOptimizer, AvailableCoolingSchedules
 
-from l2l.optimizees.functions.benchmarked_functions import BenchmarkedFunctions
-from l2l.optimizees.functions.optimizee import FunctionGeneratorOptimizee
-from l2l.utils import JUBE_runner as jube
-from l2l.paths import Paths
-import os
 
+class SAOptimizerTestCase(OptimizerTestCase):
 
-class SAOptimizerTestCase(unittest.TestCase):
-
-    def setUp(self):
-        name = "test_trajectory"
-        try:
-            with open('../../bin/path.conf') as f:
-                root_dir_path = f.read().strip()
-        except FileNotFoundError:
-            self.fail("L2L is not well configured. Missing path file.")
-        self.paths = Paths(name, dict(run_num='test'), root_dir_path=root_dir_path, suffix="-" + name)
-        self.env = Environment(
-            trajectory=name,
-            filename=".",
-            file_title='{} data'.format(name),
-            comment='{} data'.format(name),
-            add_time=True,
-            automatic_storing=True,
-            log_stdout=False,  # Sends stdout to logs
-        )
-        self.trajectory = self.env.trajectory
-        self.trajectory.f_add_parameter_group("JUBE_params", "Contains JUBE parameters")
-        self.trajectory.f_add_parameter_to_group("JUBE_params", "exec", "python3 " +
-                                      os.path.join(self.paths.simulation_path, "run_files/run_optimizee.py"))
-        self.trajectory.f_add_parameter_to_group("JUBE_params", "paths", self.paths)
-        # Test function
-        function_id = 14
-        bench_functs = BenchmarkedFunctions()
-        (benchmark_name, benchmark_function), benchmark_parameters = \
-            bench_functs.get_function_by_index(function_id, noise=True)
-
-        optimizee_seed = 1
-        self.optimizee = FunctionGeneratorOptimizee(self.trajectory, benchmark_function, seed=optimizee_seed)
-        jube.prepare_optimizee(self.optimizee, self.paths.simulation_path)
-
-    def test_setup(self):
-        parameters = SimulatedAnnealingParameters(n_parallel_runs=1, noisy_step=.03, temp_decay=.99, n_iteration=1,
-
+    def test_sa(self):
+        optimizer_parameters = SimulatedAnnealingParameters(n_parallel_runs=1, noisy_step=.03, temp_decay=.99, n_iteration=1,
                                                   stop_criterion=np.Inf, seed=np.random.randint(1e5),
                                                   cooling_schedule=AvailableCoolingSchedules.QUADRATIC_ADDAPTIVE)
 
         optimizer = SimulatedAnnealingOptimizer(self.trajectory, optimizee_create_individual=self.optimizee.create_individual,
                                                 optimizee_fitness_weights=(-1,),
-                                                parameters=parameters,
+                                                parameters=optimizer_parameters,
                                                 optimizee_bounding_func=self.optimizee.bounding_func)
-
         self.assertIsNotNone(optimizer.parameters)
-        self.env.add_postprocessing(optimizer.post_process)
 
         try:
-            self.env.run(self.optimizee.simulate)
+
+            self.experiment.run_experiment(optimizee=self.optimizee,
+                                  optimizee_parameters=self.optimizee_parameters,
+                                  optimizer=optimizer,
+                                  optimizer_parameters=optimizer_parameters)
         except Exception:
             self.fail(Exception.__name__)
-        optimizer.end(self.trajectory)
 
 
 def suite():
